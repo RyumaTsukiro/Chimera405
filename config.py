864fdcf860c997ec5c146c8b387c7201681d0a36
RyumaTsukiro <xavieritsvan@gmail.com>
2025-06-29 14:11:11 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# config.py
+
+# --- KUNCI-KUNCI WAJIB DIISI ---
+
+# Token dari @BotFather di Telegram
+TELEGRAM_TOKEN = 'PASTE_TOKEN_TELEGRAM_ANDA_DI_SINI' 
+
+# Kunci API GRATIS dari https://bscscan.com/myapikey
+BLOCK_EXPLORER_API_KEY = 'PASTE_API_KEY_BSCSCAN_ANDA_DI_SINI'
+
+# Kunci API dari https://www.virustotal.com/
+VIRUSTOTAL_API_KEY = 'PASTE_API_KEY_VIRUSTOTAL_ANDA_DI_SINI'
+
+# Kunci API dari https://debank.com/
+DEBANK_API_KEY = 'PASTE_API_KEY_DEBANK_ANDA_DI_SINI'
+
+# Kunci API dari Google AI Studio untuk Gemini
+GEMINI_API_KEY = 'PASTE_API_KEY_GEMINI_ANDA_DI_SINI'
